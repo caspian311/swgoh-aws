5ae398a9018e0212894e35fe1d10ca23153f78b9
Matt Todd <matt.c.todd@gmail.com>
2020-04-17 09:47:26 -0500

    creating launch config

--- helpers.js
+++ helpers.js
@@ -1,59 +1,53 @@
 const fs = require('fs')
 const path = require('path')
 const os = require('os')
+const AWS = require('aws-sdk')
 
-function persistKeyPair(keyData) {
-  return new Promise((resolve, reject) => {
-    const keyPath = path.join(os.homedir(), '.ssh', keyData.KeyName)
-    const options = {
-      encoding: 'utf8',
-      mode: 0o600
-    }
+AWS.config.update({
+  region: 'us-east-1'
+})
 
-    fs.writeFile(keyPath, keyData.KeyMaterial, options, (err) => {
-      if (err) reject(err)
-      else {
-        console.log('Key written to', keyPath)
-        resolve(keyData.KeyName)
-      }
-    })
-  })
-}
+const ec2 = new AWS.EC2()
+const autoScaling = new AWS.AutoScaling()
+const iam = new AWS.IAM()
 
-function createIamRole(roleName) {
-  const profileName = `${roleName}_profile`
-  const iam = new AWS.IAM()
+function createIamRole(paramsData) {
+  const profileName = `${paramsData.roleName}_profile`
   const params = {
-    RoleName: roleName,
+    RoleName: paramsData.roleName,
     AssumeRolePolicyDocument: '{ "Version": "2012-10-17", "Statement": [ { "Effect": "Allow", "Principal": { "Service": "ec2.amazonaws.com" }, "Action": "sts:AssumeRole" } ] }'
   }
 
   return new Promise((resolve, reject) => {
     iam.createRole(params, (err) => {
-      if (err) reject(err)
-      else {
+      if (err) { 
+        reject(err) 
+      } else {
         const params = {
           PolicyArn: 'arn:aws:iam::aws:policy/AdministratorAccess',
-          RoleName: roleName
+          RoleName: paramsData.roleName
         }
 
         iam.attachRolePolicy(params, (err) => {
-          if (err) reject(err)
-          else {
-            iam.createInstanceProfile({
-              InstanceProfileName: profileName
-            }, (err, iData) => {
-              if (err) reject(err)
-              else {
+          if (err) {
+            reject(err) 
+          } else {
+            iam.createInstanceProfile({ InstanceProfileName: profileName }, (err, iData) => {
+              if (err) {
+                reject(err)
+              } else {
                 const params = {
                   InstanceProfileName: profileName,
-                  RoleName: roleName
+                  RoleName: paramsData.roleName
                 }
+
                 iam.addRoleToInstanceProfile(params, (err) => {
-                  if (err) reject(err)
-                  else {
+                  if (err) { 
+                    reject(err)
+                  } else {
                     // Profile creation is slow, need to wait
-                    setTimeout(() => resolve(iData.InstanceProfile.Arn), 10000)
+                    paramsData.profileArn = iData.InstanceProfile.Arn
+                    setTimeout(() => resolve(paramsData), 10000)
                   }
                 })
               }
@@ -65,7 +59,114 @@ function createIamRole(roleName) {
   })
 }
 
+function createLaunchConfiguration(paramsData) {
+  const params = {
+      IamInstanceProfile: paramsData.profileArn,
+      ImageId: paramsData.amiImageId,
+      InstanceType: 't2.micro',
+      LaunchConfigurationName: paramsData.launchConfigurationName,
+      KeyName: paramsData.keyName,
+      SecurityGroups: [
+          paramsData.sgName
+      ],
+      UserData: paramsData.userData
+  }
+
+  return new Promise((resolve, reject) => {
+      autoScaling.createLaunchConfiguration(params, (err, data) => {
+          if (err) reject(err)
+          console.log('Launch configuration created', data)
+          resolve(paramsData)
+      })
+  })
+}
+
+function createSecurityGroup(paramsData) {
+  console.log('called createSecurityGroup with: ', paramsData)
+
+  const params = {
+      Description: paramsData.sgName,
+      GroupName: paramsData.sgName
+  }
+
+  return new Promise((resolve, reject) => {
+      ec2.createSecurityGroup(params, (err, data) => {
+          if (err) {
+            reject(err) 
+          } else {
+              console.log('security group created: ', data)
+
+              const params = {
+                  GroupId: data.GroupId,
+                  IpPermissions: [{
+                      IpProtocol: 'tcp',
+                      FromPort: 22,
+                      ToPort: 22,
+                      IpRanges: [{
+                          CidrIp: '0.0.0.0/0'
+                      }]
+                  }, {
+                      IpProtocol: 'tcp',
+                      FromPort: paramsData.port,
+                      ToPort: paramsData.port,
+                      IpRanges: [{
+                          CidrIp: '0.0.0.0/0'
+                      }]
+                  }]
+              }
+              ec2.authorizeSecurityGroupIngress(params, (err, data) => {
+                  if (err) {
+                    reject(err)
+                  } else {
+                      console.log('Ingress set on security group', data)
+                      resolve(paramsData)
+                  }
+              })
+          }
+      })
+  })
+}
+
+function createKeyPair(paramsData) {
+  const params = {
+      KeyName: paramsData.keyName
+  }
+
+  return new Promise((resolve, reject) => {
+      ec2.createKeyPair(params, (err, data) => {
+          if (err) { 
+            reject(err)
+          }  else {
+              console.log('key pair created:', data)
+              paramsData.keyData = data
+              resolve(paramsData)
+          }
+      })
+  })
+}
+
+function persistKeyPair(paramsData) {
+  return new Promise((resolve, reject) => {
+    const keyPath = path.join(os.homedir(), '.ssh', paramsData.keyData.KeyName)
+    const options = {
+      encoding: 'utf8',
+      mode: 0o600
+    }
+
+    fs.writeFile(keyPath, paramsData.keyData.KeyMaterial, options, (err) => {
+      if (err) { reject(err) 
+      } else {
+        console.log('Key written to', keyPath)
+        resolve(paramsData)
+      }
+    })
+  })
+}
+
 module.exports = {
   persistKeyPair,
-  createIamRole
+  createIamRole,
+  createSecurityGroup,
+  createKeyPair,
+  createLaunchConfiguration
 }
--- main.js
+++ main.js
@@ -1,107 +1,22 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({
-    region: 'us-east-1'
-})
+const paramsData = {
+    sgName: 'test3_sg',
+    keyName: 'test3_key',
+    roleName: 'test3_rolename',
+    amiImageId: 'ami-0323c3dd2da7fb37d',
+    launchConfigurationName: 'test3_lc',
+    port: 4567,
+    userData: 'IyEvYmluL2Jhc2gKCiMgZm9yIGluc3RhbmNlIElEIGFtaS0wMzIzYzNkZDJkYTdmYjM3ZAoKeXVtIGluc3RhbGwgLXkgZ2l0IGRvY2tlcgpzZXJ2aWNlIGRvY2tlciBzdGFydApkb2NrZXIgcHVsbCBydWJ5CgpnaXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL2Nhc3BpYW4zMTEvc3dnb2gtYXBwLmdpdCAvcm9vdC9zd2dvaC1hcHAKZG9ja2VyIHJ1biAtLXJtIC12IC9yb290L3N3Z29oLWFwcDovYXBwIC1wIDQ1Njc6NDU2NyBydWJ5IC9hcHAvc3RhcnR1cC5zaA=='
+}
 
-const ec2 = new AWS.EC2()
-const sgName = 'test3_sg'
-const keyName = 'test3_key'
-
-
-
-createSecurityGroup(sgName, 4567)
-    .then(() => {
-        return createKeyPair(keyName)
-    })
+helpers.createKeyPair(paramsData)
     .then(helpers.persistKeyPair)
-    .then(() => {
-        return createInstance(sgName, keyName)
-    })
-    .catch((message) => {
+    .then(helpers.createSecurityGroup)
+    .then(helpers.createIamRole)
+    .then(helpers.createLaunchConfiguration)
+    .then(data => console.log(data))
+    .catch(message => {
         console.log('Failed...', message)
     })
-
-function createSecurityGroup(sgName, port) {
-    const params = {
-        Description: sgName,
-        GroupName: sgName
-    }
-
-    return new Promise((resolve, reject) => {
-        ec2.createSecurityGroup(params, (err, data) => {
-            if (err) reject(err)
-            else {
-                console.log('security group created: ', data)
-
-                const params = {
-                    GroupId: data.GroupId,
-                    IpPermissions: [{
-                        IpProtocol: 'tcp',
-                        FromPort: 22,
-                        ToPort: 22,
-                        IpRanges: [{
-                            CidrIp: '0.0.0.0/0'
-                        }]
-                    }, {
-                        IpProtocol: 'tcp',
-                        FromPort: port,
-                        ToPort: port,
-                        IpRanges: [{
-                            CidrIp: '0.0.0.0/0'
-                        }]
-                    }]
-                }
-                ec2.authorizeSecurityGroupIngress(params, (err, data) => {
-                    if (err) reject(err)
-                    else {
-                        console.log('Ingress set on security group', data)
-                        resolve()
-                    }
-
-                })
-            }
-        })
-    })
-}
-
-function createKeyPair(keyName) {
-    const params = {
-        KeyName: keyName
-    }
-
-    return new Promise((resolve, reject) => {
-        ec2.createKeyPair(params, (err, data) => {
-            if (err) reject(err)
-            else {
-                console.log('key pair created:', data)
-                resolve(data)
-            }
-        })
-    })
-}
-
-function createInstance(sgName, keyName) {
-    const params = {
-        ImageId: 'ami-0323c3dd2da7fb37d',
-        InstanceType: 't2.micro',
-        KeyName: keyName,
-        MaxCount: 1,
-        MinCount: 1,
-        SecurityGroups: [
-            sgName
-        ],
-        UserData: 'IyEvYmluL2Jhc2gKCiMgZm9yIGluc3RhbmNlIElEIGFtaS0wMzIzYzNkZDJkYTdmYjM3ZAoKeXVtIGluc3RhbGwgLXkgZ2l0IGRvY2tlcgpzZXJ2aWNlIGRvY2tlciBzdGFydApkb2NrZXIgcHVsbCBydWJ5CgpnaXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL2Nhc3BpYW4zMTEvc3dnb2gtYXBwLmdpdCAvcm9vdC9zd2dvaC1hcHAKZG9ja2VyIHJ1biAtLXJtIC12IC9yb290L3N3Z29oLWFwcDovYXBwIC1wIDQ1Njc6NDU2NyBydWJ5IC9hcHAvc3RhcnR1cC5zaA=='
-    }
-
-    return new Promise((resolve, reject) => {
-        ec2.runInstances(params, (err, data) => {
-            if (err) reject(err)
-            else { 
-                console.log('Created EC2 instance', data)
-                resolve(data)
-            }
-        })
-    })
-}
